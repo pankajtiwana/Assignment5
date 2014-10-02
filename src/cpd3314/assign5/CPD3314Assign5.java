70992c13b6cd46a026827e03a368ae9f8de08488
Len Payne <len.payne@lambtoncollege.ca>
2014-10-02 15:01:40 -0400

    Initial Commit - Framework for Questions Built

---
+++ src/cpd3314/assign5/CPD3314Assign5.java
@@ -0,0 +1,139 @@
+/* 
+ * Copyright 2014 Len Payne <len.payne@lambtoncollege.ca>. 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */ 
+
+/* The following exercises are adapted from: 
+ * Gaddis, T. (2013). Starting Out with Java: From Control Structures through  
+ * Objects. (5th ed.). Upper Saddle River, NJ: Pearson Education. 
+ * ISBN: 978-0-13-285583-9 
+ */ 
+
+package cpd3314.assign5;
+
+/**
+ *
+ * @author <ENTER YOUR NAME HERE>
+ */
+public class CPD3314Assign5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    /*
+     * Exercise #2 - Retail Price Calculator
+     * 
+     * Write a program that asks the user to enter an item's wholesale cost and 
+     * its markup percentage. It should then display the item's retail price. 
+     * For example:
+     *   - If an item's wholesale cost is 5.00 and its markup percentage is 100 
+     *       percent, then the item's retail price is 10.00.
+     *   - If an item's wholesale cost is 5.00 and its markup percentage is 50 
+     *       percent, then the item's retail price is 7.50.
+     * The program should reference a method named calculateRetail that receives 
+     * the wholesale cost and the markup percentage as arguments, and returns 
+     * the retail price of the item.
+     * - Gaddis pg. 316
+     */
+    // TODO: Build the calculateRetail method here
+    public static void doExercise2() {
+        // TODO: Complete the rest of Exercise #2 here to test calculateRetail
+
+    }
+
+    /*
+     * Exercise #4 - Paint Job Estimator
+     * 
+     * A painting company has determined that for every 115 square feet of wall 
+     * space, one gallon of paint and eight hours of labour will be required. 
+     * The company charges $18.00 per hour for labour. Write a program that 
+     * allows the user to enter the number of rooms to be painted and the price 
+     * of the paint per gallon. It should also ask for the square feet of wall 
+     * space in each room. The program should have methods with the following 
+     * names that return the following data:
+     *   - numGallons: The number of gallons of paint required
+     *   - hoursLabour: The hours of labour required
+     *   - costPaint: The cost of the paint
+     *   - labourCharges: The labour charges
+     *   - totalCost: The total cost of the paint job
+     * Then it should display the data on the screen.
+     * - Gaddis pg. 316-7
+     */
+    // TODO: Build the numGallons method here
+    // TODO: Build the hoursLabour method here
+    // TODO: Build the costPain method here
+    // TODO: Build the labourCharges method here
+    // TODO: Build the totalCost method here
+    public static void doExercise4() {
+        // TODO: Build the rest of exercise #4 here to test the above methods
+
+    }
+
+    /*
+     * Exercise #9 - Distance Traveled Modification
+     * 
+     * The distance a vehicle travels can be calculated as follows:
+     *   Distance = Speed * Time
+     * Write a method named distance that accepts a vehicle's speed and time as 
+     * arguments, and returns the distance the vehicle has traveled. Modify the 
+     * "Distance Traveled" program you wrote in Assignment #4 to use this 
+     * method.
+     * - Gaddis pg. 319
+     */
+    // TODO: Build the distance method here
+    public static void doExercise9() {
+        // TODO: Build the rest of exercise #9 here to test the distance method
+
+    }
+
+    /*
+     * Exercise #13 - isPrime Method
+     * 
+     * A prime number is a number that is evenly divisible only by itself and 1. 
+     * For example, the number 5 is prime because it can be evenly divided only 
+     * by 1 and 5. The number 6, however, is not prime because it can be divided 
+     * evenly by 1, 2, 3, and 6.
+     * Write a method named isPrime, which takes an integer as an argument and 
+     * returns true if the argument is a prime number, or false otherwise.
+     * - Gaddis pg. 320
+     */
+    // TODO: Build the isPrime method here
+    public static void doExercise13() {
+        // TODO: Build a sample program to test the isPrime method here
+
+    }
+
+    /*
+     * Exercise #15 - Even/Odd Counter
+     * 
+     * You can use the following logic to determine if a number is even or odd:
+     *   if ((number % 2) == 0) {
+     *     // The number is even
+     *   }
+     *   else {
+     *     // The number is odd
+     *   }
+     * Write a program method named isEven that accepts an int argument. The 
+     * method should return true if the argument is even, or false otherwise.
+     */
+    // TODO: Build the isEven method here
+    public static void doExercise15() {
+        // TODO: Build a sample program to test the isEven method here
+
+    }
+}
